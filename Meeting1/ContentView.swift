a2266d6393beb983ab7ce15cc96f18f6e7e28225
Perizat <perizatmoldabek1@gmail.com>
2025-12-11 08:46:51 +0500

    Initial Commit

---
+++ Meeting1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Meeting1
+//
+//  Created by Nurislam Yerkinuly on 11.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
